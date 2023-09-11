df48a4a718b78fb3ad2fbdcb6fef7a1dfebde5d1
Vikram Gupta <basecs101@gmail.com>
2023-09-11 20:23:09 +0530

    Comparator example using stream and multiple comparison

--- comparable-example/src/ComparatorExample.java
+++ comparable-example/src/ComparatorExample.java
@@ -1,12 +1,15 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Employee {
     String name;
     int id;
+    int salary;
 
-    public Employee(String name, int id) {
+    public Employee(String name, int id, int salary) {
         this.name = name;
         this.id = id;
+        this.salary = salary;
     }
 
     public String getName() {
@@ -25,27 +28,32 @@ class Employee {
         this.id = id;
     }
 
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-
-        if (o instanceof Employee) {
-            Employee employee = (Employee) o;
-            return this.id == employee.id && this.name.equals(employee.name);
-        } else
-            return false;
+        if (!(o instanceof Employee employee)) return false;
+        return id == employee.id && salary == employee.salary && Objects.equals(name, employee.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, id);
+        return Objects.hash(name, id, salary);
     }
 
     @Override
     public String toString() {
-        return "Student{" +
+        return "Employee{" +
                 "name='" + name + '\'' +
                 ", id=" + id +
+                ", salary=" + salary +
                 '}';
     }
 }
@@ -60,7 +68,9 @@ class CompareById implements Comparator<Employee> {
 class CompareByName implements Comparator<Employee> {
     @Override
     public int compare(Employee o1, Employee o2) {
-        return o1.name.equals(o2.name) ? 0 : 1;
+        Comparator<String> stringComparator = Comparator.naturalOrder();
+
+        return stringComparator.compare(o1.name, o2.name);
     }
 }
 public class ComparatorExample {
@@ -68,17 +78,42 @@ public class ComparatorExample {
     public static void main(String[] args) {
 
         List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(new Employee("Vikram",100));
-        employeeList.add(new Employee("Karan",400));
-        employeeList.add(new Employee("Moin",500));
-        employeeList.add(new Employee("Pratik",200));
-        employeeList.add(new Employee("Komal",300));
+        employeeList.add(new Employee("Vikram",100, 130000));
+        employeeList.add(new Employee("Karan",100, 120000));
+        employeeList.add(new Employee("Karan",100, 110000));
+        employeeList.add(new Employee("Moin",500, 300000));
+        employeeList.add(new Employee("Pratik",200, 120000));
+        employeeList.add(new Employee("Komal",100, 100000));
+
 
         System.out.println("before sorting");
         employeeList.forEach(System.out::println);
         CompareById compareById = new CompareById();
         Collections.sort(employeeList,compareById);
-        System.out.println("after sorting");
+        System.out.println("after sorting on ids");
         employeeList.forEach(System.out::println);
+        CompareByName compareByName = new CompareByName();
+        Collections.sort(employeeList, compareByName);
+        System.out.println("after sorting on names");
+        employeeList.forEach(System.out::println);
+
+        //comparator example using streams
+        System.out.println("Compare(id) and thenComparing(name) ");
+        //Compare on the basis of id
+//        Comparator<Employee> idComparator = Comparator.comparingInt(Employee::getId);
+//        // Use thenComparing to add secondary sorting by name
+//        Comparator<Employee> nameComparator = idComparator.thenComparing(Employee::getSalary);
+//        // Use thenComparing to add third sorting by name
+//        Comparator<Employee> salaryComparator = nameComparator.thenComparing(Employee::getSalary);
+
+        // Sort the list of employee using the combined comparator
+        List<Employee> sortedEmployee = employeeList.stream()
+                .sorted(Comparator.comparing(Employee::getId)
+                        .thenComparing(Employee::getName)
+                        .thenComparing(Employee::getSalary))
+                .toList();
+
+        sortedEmployee.forEach(System.out::println);
+
     }
 }
